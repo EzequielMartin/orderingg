354cd1e70b7113ad31151a28d492b297a62c7fe7
Ezequiel <ezequielmartin92@gmail.com>
2018-05-26 03:50:19 -0300

    Agrego test de integracion para borrar una fila de la orden

--- test/test_e2e.py
+++ test/test_e2e.py
@@ -4,6 +4,7 @@ import time
 import threading
 
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 from app import create_app, db
 from app.models import Product, Order, OrderProduct
@@ -38,6 +39,7 @@ class Ordering(unittest.TestCase):
 
         self.driver = webdriver.Chrome()
 
+    '''
     def test_title(self):
         driver = self.driver
         driver.get(self.baseURL)
@@ -45,6 +47,27 @@ class Ordering(unittest.TestCase):
         add_product_button.click()
         modal = driver.find_element_by_id('modal')
         assert modal.is_displayed(), "El modal no esta visible"
+    '''
+    
+    def test_delete(self):
+
+        o = Order(id= 1)
+        db.session.add(o)
+
+        p = Product(id= 1, name= 'Tenedor', price= 50)
+        db.session.add(p)
+
+        orderProduct = OrderProduct(order_id= 1, product_id= 1, quantity= 1, product= p)
+        db.session.add(orderProduct)
+        db.session.commit()
+
+        driver = self.driver
+        driver.get(self.baseURL)
+        
+        delete_product_button = driver.find_element_by_xpath('/html/body/main/div[2]/div/table/tbody/tr[1]/td[6]/button[2]')
+        delete_product_button.click()   
+        self.assertRaises(NoSuchElementException, driver.find_element_by_xpath('/html/body/main/div[2]/div/table/tbody/tr[1]').click())
+
 
     def tearDown(self):
         self.driver.get('http://localhost:5000/shutdown')
