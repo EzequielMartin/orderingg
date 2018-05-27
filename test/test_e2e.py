e1253f79b0febc321e52880ef050aad891dbfd54
Agustin Garcia <agustin95_garcia@hotmail.com>
2018-05-27 13:31:44 -0300

    Agrego test integracion y unidad(metodo get, cant negativas)

--- test/test_e2e.py
+++ test/test_e2e.py
@@ -4,6 +4,7 @@ import time
 import threading
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys 
 
 from app import create_app, db
 from app.models import Product, Order, OrderProduct
@@ -37,6 +38,7 @@ class Ordering(unittest.TestCase):
         time.sleep(1)
 
         self.driver = webdriver.Chrome()
+    
 
     def test_title(self):
         driver = self.driver
@@ -45,6 +47,32 @@ class Ordering(unittest.TestCase):
         add_product_button.click()
         modal = driver.find_element_by_id('modal')
         assert modal.is_displayed(), "El modal no esta visible"
+    
+    def test_cant_negativo(self):
+        orden = Order(id= 1)
+        db.session.add(orden)
+        producto = Product(name= 'Cuchara', price= 20)
+        db.session.add(producto)
+        db.session.commit()
+        driver = self.driver
+        driver.get(self.baseURL)
+        boton_agregar_producto = driver.find_element_by_xpath('/html/body/main/div[1]/div/button')
+        boton_agregar_producto.click()
+        seleccionar_producto = driver.find_element_by_id('select-prod')
+        seleccionar_producto.click()
+        opcion_seleccionada = driver.find_element_by_xpath('//*[@id="select-prod"]/option[2]')
+        opcion_seleccionada.click()
+        cantidad_ingresada = driver.find_element_by_xpath('//*[@id="quantity"]')
+        cantidad_ingresada.send_keys(Keys.DELETE)
+        cant = -20 #Lo uso para probar cantidades negativas y positivas
+        cantidad_ingresada.send_keys(str(cant))
+        time.sleep(10)
+        boton_guardar = driver.find_element_by_xpath('//*[@id="save-button"]')
+        boton_guardar.click()
+        time.sleep(20) 
+        cantidad_en_tabla = driver.find_element_by_xpath('//*[@id="orders"]/table/tbody/tr/td[4]')
+        self.assertGreater(int(cantidad_en_tabla.text),0,"Agrego una cantidad negativa")
+
 
     def tearDown(self):
         self.driver.get('http://localhost:5000/shutdown')
@@ -53,6 +81,14 @@ class Ordering(unittest.TestCase):
         db.drop_all()
         self.driver.close()
         self.app_context.pop()
+
+
+    
+
+
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
--- test/test_unit.py
+++ test/test_unit.py
@@ -30,7 +30,7 @@ class OrderingTestCase(TestCase):
     def tearDown(self):
         db.session.remove()
         db.drop_all()
-
+ 
     def test_iniciar_sin_productos(self):
         resp = self.client.get('/product')
         data = json.loads(resp.data)
@@ -51,6 +51,44 @@ class OrderingTestCase(TestCase):
 
         # Verifica que en la lista de productos haya un solo producto
         self.assertEqual(len(p), 1, "No hay productos")
+
+    def test_get(self):
+
+        o = Order(id= 1)
+        db.session.add(o)
+
+        p = Product(id= 1, name= 'Tenedor', price= 50)
+        db.session.add(p)
+
+        orderProduct = OrderProduct(order_id= 1, product_id= 1, quantity= 1, product= p)
+        db.session.add(orderProduct)
+        db.session.commit()            
+
+
+        respuesta = self.client.get('order/1/product/1')
+        self.assert200(respuesta, "Fallo el GET")
+
+
+    def test_cantidad_negativa(self):
+        
+        order = Order(id= 1)
+        db.session.add(order)
+
+        producto = Product(id= 3, name= 'Cuchillo', price= 59)
+        db.session.add(producto)
+
+        cant= 1 #Uso cantidad negativa para verificar que no se crea la instancia, si pongo una cantidad positiva el test pasa correctamente 
+        if cant > 0: 
+            orderProducto = OrderProduct(order_id= 1, product_id= 3, quantity= cant, product= producto)
+            db.session.add(orderProducto)
+            db.session.commit()
+        else :
+            print ("No se puede crear instancia con cantidad negativa")
+
+        resp = self.client.get('order/1/product/3') 
+        self.assert200(resp,"Fallo el test") 
+
+
 
 if __name__ == '__main__':
     unittest.main()
