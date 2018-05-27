d471871518bed443f18cc804d0c05ec0a63fb778
Matías Fontana Mac Adden <mati.macadden@gmail.com>
2018-05-26 21:29:25 -0300

    Agrego los tests de PUT, orderPrice, informacion del modal

--- test/test_e2e.py
+++ test/test_e2e.py
@@ -37,7 +37,7 @@ class Ordering(unittest.TestCase):
         time.sleep(1)
 
         self.driver = webdriver.Chrome()
-
+    '''
     def test_title(self):
         driver = self.driver
         driver.get(self.baseURL)
@@ -45,6 +45,34 @@ class Ordering(unittest.TestCase):
         add_product_button.click()
         modal = driver.find_element_by_id('modal')
         assert modal.is_displayed(), "El modal no esta visible"
+    '''
+    def test_InfoModalEditar(self):
+        o = Order(id= 1)
+        db.session.add(o)
+
+        p = Product(id= 1, name= 'Tenedor', price= 50)
+        db.session.add(p)
+
+        orderProduct = OrderProduct(order_id= 1, product_id= 1, quantity= 1, product= p)
+        db.session.add(orderProduct)
+        db.session.commit()
+
+        driver = self.driver
+        driver.get(self.baseURL)
+        time.sleep(5)
+
+        edit_product_button = driver.find_element_by_xpath('/html/body/main/div[2]/div/table/tbody/tr[1]/td[6]/button[1]')
+        edit_product_button.click()
+
+        producto = driver.find_element_by_xpath('//*[@id="select-prod"]')
+        cantidad = driver.find_element_by_xpath('//*[@id="quantity"]')
+        value_prod = producto.get_attribute("value")
+        value_cant = cantidad.get_attribute("value")
+        boton_cerrar_modal = driver.find_element_by_xpath('//*[@id="modal"]/div[2]/footer/button[3]')
+        time.sleep(5)
+        boton_cerrar_modal.click()
+        self.assertTrue(value_prod != "", "No tiene informacion")
+        self.assertTrue(value_cant != "", "No tiene informacion")
 
     def tearDown(self):
         self.driver.get('http://localhost:5000/shutdown')
